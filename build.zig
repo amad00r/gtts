b37fe26a30c120b7f3a6600d2e2d2559c6d0c852
Amadeu Moya <amadeu.moya@estudiantat.upc.edu>
2025-02-27 19:06:11 +0100

    Fix build.zig

--- build.zig
+++ build.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    _ = b.addModule("gtts", .{ .root_source_file = b.path("src/gtts.zig") });
+
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -11,29 +13,20 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    _ = b.addModule("gtts", .{
+    b.installArtifact(exe);
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    const tests = b.addTest(.{
         .root_source_file = b.path("src/gtts.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
+    const run_test = b.addRunArtifact(tests);
+    run_test.step.dependOn(b.getInstallStep());
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
+    test_step.dependOn(&run_test.step);
 }
